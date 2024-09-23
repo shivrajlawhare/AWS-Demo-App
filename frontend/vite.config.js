c7875de91ca8754cbb2d4a532b3e2ad205ef9a05
shivrajlawhare <shivrajlawhare6@gmail.com>
2024-09-24 02:14:16 +0530

    Remove .env file from the repository

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -8,3 +11,4 @@ export default defineConfig({
     // },
   },
 });
+
